09af08f975992169cc876beee41b6a0e64969974
Chip <emailmuahang@gmail.com>
2014-07-16 15:44:42 +0700

    copy to other file create lots of error, at least 2 files

--- GetChineseWords/fromXml/getChinese.js
+++ GetChineseWords/fromXml/getChinese.js
@@ -1,85 +1,84 @@
 
 function getChinese() {
 
-    walkFile(dirPath, extension, function (contents, file) {
+    walkFile(dirPath, extension, parseFile2);
 
-        util.debug(getShortPath(file));
+}
 
-        wordCount = 0;
-        var firstChineseOfFile = true;
+var firstChineseOfFile = true;
+var file;
 
-        xmlParser.parseString(contents, function (err, result) {
+function parseFile2(contents, _file) {
 
-            if(err) util.debug(err);
+    file = _file;
 
-            walkXml(result, function (word, object, property, type) {
+    util.debug(getShortPath(file));
 
-                // if end of xml file, write file to disk ONLY IN PUT MODE
-                if (word === 'EoF_EoF' && !config.getWordMode) {
+    wordCount = 0;
+    firstChineseOfFile = true;
 
-                    var builder = new xml2js.Builder({renderOpts: { 'pretty': true, 'indent': '  ', 'newline': '\n' }});
-                    var xml = builder.buildObject(result);
-//                    util.debug(xml);
+    xmlParser.parseString(contents, parseXmlFile);
+    xmlParser.reset();
+}
 
-                    var xml2 = unescapeHTML(xml);
+var word, object, property, type;
 
-                    createFile(dirPath+'_output'+getShortPath(file), xml2);
-                }
+function parseXmlFile(err, result) {
 
-                // for every word found in xml, check if chinese
-                checkChinese(word, function (isChinese) {
+    if(err) util.debug(err);
+    else walkXml(result, function (_word, _object, _property, _type) {
 
-                    if(isChinese) {
+        word = _word;
+        object = _object;
+        property = _property;
+        type = _type;
 
-                        // extract mode
-                        if (config.getWordMode) {
+        // for every word found in xml, check if chinese
+        checkChinese(word, isChinese);
+    });
+}
 
-                            if (characterCount > config.characterLimit) {
+function isChinese() {
 
-                                createFile("D:\\json\\json" + fileCount + ".txt", string);
 
-                                fileCount++;
-                                string = '';
-                                chineseFiles = [];
+    // extract mode
+    if (config.getWordMode) {
 
-                                characterCount = 0;
-                            }
+        if (characterCount > config.characterLimit) {
+
+            createFile("D:\\json\\json" + fileCount + ".txt", string);
+
+            fileCount++;
+            string = '';
+            chineseFiles = [];
+
+            characterCount = 0;
+        }
 
 //                            if (firstChineseOfFile) {
 
-                            var shortPath = getShortPath(file);
-                            chineseFiles.push(new File(shortPath));
+        var shortPath = getShortPath(file);
+        chineseFiles.push(new File(shortPath));
 
 //                                var _string = shortPath + '\n';
-                            var _string = shortPath + '\t';
+        var _string = shortPath + '\t';
 
-                            characterCount += _string.length;
-                            string += _string;
+        characterCount += _string.length;
+        string += _string;
 
-                            firstChineseOfFile = false;
+        firstChineseOfFile = false;
 //                            }
 
-                            chineseFiles[chineseFiles.length - 1].Words.push({i: wordCount, w: escapeHtml(word)});
+        chineseFiles[chineseFiles.length - 1].Words.push({i: wordCount, w: escapeHtml(word)});
 
 //                            _string = wordCount + '\nw":"' + word + '"\n';
-                            _string = wordCount + '\t' + word + '\n';
+        _string = wordCount + '\t' + word + '\n';
 
-                            characterCount += _string.length;
-                            string += _string;
+        characterCount += _string.length;
+        string += _string;
 
 
-                        }
-
-                    }else{
-                        if(type === 'content'){
-                            object[property] = word;
-                        }
-                    }
-                });
-            });
-        });
-        xmlParser.reset();
-    });
+    }
 
 }
 
