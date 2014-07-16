b8f43d585b10cafde1db43ce35634d197ba0f0e8
Chip <emailmuahang@gmail.com>
2014-07-16 09:44:19 +0700

    refactoring - not working

--- GetChineseWords/fromXml/app.js
+++ GetChineseWords/fromXml/app.js
@@ -3,37 +3,7 @@ function initialize() {
 // read file only once
     if (!config.getWordMode) {
 
-        readFile(config.filePath, function (contents) {
-
-//        var _contents = contents.replace('\r\n', '\n').replace('\r','');
-            var _contents = contents;
-
-            var lines = _contents.split('\n');
-
-            lines.forEach(function (line) {
-                var entries = line.split('\t');
-
-                if (entries.length !== 4) util.debug('length !== 4');
-
-                // REMEMBER TO SORT THE EXCEL FILE FIRST
-                // if array is empty OR if current path is different from the last path
-                if (vietnameseFiles.length === 0 || vietnameseFiles[vietnameseFiles.length - 1].path !== entries[0]) {
-                    vietnameseFiles.push(new File(entries[0]));
-                }
-                if (typeof entries[3] !== 'undefined') {
-                    var word = entries[3];
-                    var word2 = word.replace(/\r/g, '');
-                } else {
-                    word2 = entries[3];
-                }
-
-                vietnameseFiles[vietnameseFiles.length - 1].Words.push({i: entries[1], zh: entries[2], vn: word2});
-            });
-
-//        util.debug(JSON.stringify(vietnameseFiles, undefined, 2));
-
-            replaceChineseWithVietnamese();
-        });
+        readFile(config.filePath, parseFile);
 
         alreadyReadFile = true;
     } else getChinese();
@@ -41,6 +11,110 @@ function initialize() {
 }
 
 
+function parseFile(contents) {
+
+//        var _contents = contents.replace('\r\n', '\n').replace('\r','');
+    var _contents = contents;
+
+    var lines = _contents.split('\n');
+
+    lines.forEach(parseLine);
+
+//        util.debug(JSON.stringify(vietnameseFiles, undefined, 2));
+
+    replaceChineseWithVietnamese();
+}
+
+function parseLine(line) {
+    var entries = line.split('\t');
+
+    if (entries.length !== 4) util.debug('length !== 4');
+
+    // REMEMBER TO SORT THE EXCEL FILE FIRST
+    // if array is empty OR if current path is different from the last path
+    if (vietnameseFiles.length === 0 || vietnameseFiles[vietnameseFiles.length - 1].path !== entries[0]) {
+        vietnameseFiles.push(new File(entries[0]));
+    }
+    if (typeof entries[3] !== 'undefined') {
+        var word = entries[3];
+        var word2 = word.replace(/\r/g, '');
+    } else {
+        word2 = entries[3];
+    }
+
+    vietnameseFiles[vietnameseFiles.length - 1].Words.push({i: entries[1], zh: entries[2], vn: word2});
+}
+
+function getChinese() {
+
+    walkFile(dirPath, extension, parseFile2);
+
+}
+
+function parseFile2(contents, file) {
+
+    util.debug(getShortPath(file));
+
+    wordCount = 0;
+    var firstChineseOfFile = true;
+
+    xmlParser.parseString(contents, parseXmlFile);
+    xmlParser.reset();
+}
+
+function parseXmlFile(err, result) {
+
+    if(err) util.debug(err);
+    else walkXml(result, function (word, object, property, type) {
+
+        // for every word found in xml, check if chinese
+        checkChinese(word, isChinese);
+    });
+}
+
+function isChinese() {
+
+
+    // extract mode
+    if (config.getWordMode) {
+
+        if (characterCount > config.characterLimit) {
+
+            createFile("D:\\json\\json" + fileCount + ".txt", string);
+
+            fileCount++;
+            string = '';
+            chineseFiles = [];
+
+            characterCount = 0;
+        }
+
+//                            if (firstChineseOfFile) {
+
+        var shortPath = getShortPath(file);
+        chineseFiles.push(new File(shortPath));
+
+//                                var _string = shortPath + '\n';
+        var _string = shortPath + '\t';
+
+        characterCount += _string.length;
+        string += _string;
+
+        firstChineseOfFile = false;
+//                            }
+
+        chineseFiles[chineseFiles.length - 1].Words.push({i: wordCount, w: escapeHtml(word)});
+
+//                            _string = wordCount + '\nw":"' + word + '"\n';
+        _string = wordCount + '\t' + word + '\n';
+
+        characterCount += _string.length;
+        string += _string;
+
+
+    }
+
+}
 
 ////////////////////////////////////////////////////////////
 
@@ -51,13 +125,13 @@ var config = require('./config'),
     xmlParser = xml2js.Parser(),
     escapeHtml = require('./escapeHtml').escapeHtml,
     util = require('util'),
-    replaceChineseWithVietnamese = require('./replaceChineseWithVietnamese').replaceChineseWithVietnamese,
-    getChinese = require('./getChinese').getChinese;
+//    getChinese = require('./getChinese').getChinese,
+    replaceChineseWithVietnamese = require('./replaceChineseWithVietnamese').replaceChineseWithVietnamese;
 
 var app = require('express')();
 var http = require('http').Server(app);
 app.get('/', function (req, res) {
-    res.sendfile('fromXml/index.html');
+    res.sendfile('./fromXml/index.html');
 });
 app.get('/jquery-1.10.2.js', function (req, res) {
     res.sendfile('fromXml/jquery-1.10.2.js');
@@ -95,14 +169,9 @@ function checkChinese(word, callback) {
 
     if (word.toString().match(/[\u3400-\u9FBF]/)) {
 
-        callback(true);
-    } else {
-        callback(false);
+        callback();
     }
     wordCount++;
-
-    // find a word that unique to seperate
-    if (word.toString().match('`')) console.log(word);
 }
 
 function createFile(path, content){
